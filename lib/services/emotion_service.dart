b7eb28080d4b89489ed913d362c3a38944e3fbcf
shshivam1520-dot <shshivam1520@gmail.com>
2026-02-07 08:59:03 +0530

    Create emotion_service.dart

---
+++ lib/services/emotion_service.dart
@@ -0,0 +1,10 @@
+class EmotionService {
+  Map<String, int> analyzeEmotion(String audioData) {
+    return {
+      "happiness": 50,
+      "anger": 10,
+      "fear": 5,
+      "sadness": 35,
+    };
+  }
+}
